af3721009a9290b18672610fa5053016174a697b
KylerO <kylerolsen17@gmail.com>
2025-04-11 11:24:07 -0600

    Started data loading

--- library.py
+++ library.py
@@ -4,6 +4,7 @@
 
 from __future__ import annotations
 from typing import TYPE_CHECKING
+import json
 
 if TYPE_CHECKING:
     from game import Game
@@ -12,13 +13,23 @@ if TYPE_CHECKING:
 class Library:
 
     __verses: dict
-    __games: Game
+    __games: list[Game]
 
     def __init__(self):
-        pass
+        with open("data/scripture-frequencies.json", encoding='utf-8') as file:
+            self.__verses = json.load(file)
+        self.__games = []
 
-    def join_game(self, name: str):
-        pass
+    def join_game(self, name: str, game_num: int = -1):
+        if game_num == -1:
+            for i, game in enumerate(self.__games):
+                if not game.active:
+                    game_num = i
+                    break
+            else:
+                self.__games.append(Game())
+                game_num = len(self.__games) - 1
+        self.__games[game_num].add_player(name)
 
     def get_verse(self, difficulty: int):
         pass
